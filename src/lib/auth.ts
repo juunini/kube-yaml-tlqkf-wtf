887c65aee5cf22ec9d20ebeb7178e1476bbf906b
Juunini <juuni.ni.i@gmail.com>
2023-05-05 16:50:34 +0900

    chore: add deploy workflow

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,4 @@
 import type { GitHubUser } from './components/posts/interfaces';
-import { BACKEND_URL } from './config';
 
 export function gitHubUserInfo(): GitHubUser | null {
 	const userInfo = localStorage.getItem('GitHubUserInfo');
@@ -9,18 +8,4 @@ export function gitHubUserInfo(): GitHubUser | null {
 	}
 
 	return JSON.parse(userInfo) as GitHubUser;
-}
-
-export async function gitHubLogin(code: string, state?: string): Promise<void> {
-	const response = await fetch(`${BACKEND_URL}/api/auth/github/login`, {
-		method: 'POST',
-		body: JSON.stringify({ code })
-	});
-	const userInfo = await response.json();
-
-	localStorage.setItem('GitHubUserInfo', JSON.stringify(userInfo));
-
-	if (state !== undefined) {
-		location.href = decodeURIComponent(state);
-	}
 }
