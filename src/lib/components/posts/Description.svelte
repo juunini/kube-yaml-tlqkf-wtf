380ae50c3797761303bd56881cb2ba32456d754e
Juunini <juuni.ni.i@gmail.com>
2023-05-07 14:32:38 +0900

    feat: description changes pre to markdown

--- src/lib/components/posts/Description.svelte
+++ src/lib/components/posts/Description.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+	import { marked } from 'marked';
+
 	export let editable: boolean = false;
 	export let description: string;
 </script>
 
 {#if editable}
-	<textarea class="textarea" placeholder="Insert Description here..." bind:value={description} />
+	<div class="grid grid-cols-2">
+		<textarea class="textarea" placeholder="Insert Description here..." bind:value={description} />
+		<div class="card p-2">{@html marked.parse(description, { breaks: true })}</div>
+	</div>
 {:else}
-	<pre class="mb-6">{description}</pre>
+	<div class="mb-6">{@html marked.parse(description, { breaks: true })}</div>
 {/if}
